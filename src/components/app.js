4d12973f6006c07ca1a352c88297932f267b7b17
Richard Waswrick <richard.waswrick@ppdbiz.com>
2018-10-04 21:03:40 -0500

    new login form

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header/index';
 import Footer from './footer/index';
+import Login from './login/index'
 
 export default class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
         <Header/>
         <div class="row">
             <div Id="content" Class="col-md-12" Style="padding-left:30px;padding-right:30px;padding-top:20px">
+              <Login/>
             </div>
         </div>
         <Footer/>
