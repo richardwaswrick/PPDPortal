8cf8dbebab960d629aac579f3b20faac12d9aafb
Richard Waswrick <richard.waswrick@ppdbiz.com>
2018-10-06 12:07:47 -0500

    udpated for jest testing

--- test/header.test.js
+++ test/header.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '../src/components/header/App';
+import Header from '../src/components/header';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Header />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
