1da04154acfe0a3865a26cca616d3a7d1ab6ca04
Richard Waswrick <richard.waswrick@ppdbiz.com>
2018-10-06 15:06:46 -0500

    converted arrow functions

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,63 +1,87 @@
 import React from 'react';
 import { Formik } from 'formik';
+import { Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import * as Yup from 'yup';
 
-const Basic = () => (
-  <div>
-    <h1>Anywhere in your app!</h1>
-    <Formik
-      initialValues={{ email: '', password: '' }}
-      validate={values => {
-        let errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = 'Invalid email address';
-        }
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        /* and other goodies */
-      }) => (
-        <form onSubmit={handleSubmit}>
-          <input
-            type="email"
-            name="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.email}
-          />
-          {errors.email && touched.email && errors.email}
-          <input
-            type="password"
-            name="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.password}
-          />
-          {errors.password && touched.password && errors.password}
-          <button type="submit" disabled={isSubmitting}>
-            Submit
-          </button>
-        </form>
-      )}
-    </Formik>
-  </div>
-);
+export default class Login extends React.Component {
+  render() {
+    
+    const validationSchema = Yup.object().shape({
+      email: Yup.string()
+        .email('E-mail is not valid!')
+        .required('E-mail is required!'),
+      password: Yup.string()
+        .min(6, 'Password has to be longer than 6 characters!')  
+        .required('Password is required!')
+    });
 
-export default Basic;
+    function onSubmit(values, { setSubmitting }) {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        setSubmitting(false);
+      }, 400);
+    }
+
+    return (  
+      <div>
+        <h3>Login</h3>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          validationSchema = {validationSchema}
+          onSubmit={onSubmit}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            dirty,
+            /* and other goodies */
+          }) => (
+            <Form onSubmit={handleSubmit}>
+              <Row form>
+                <Col md={4}>
+                  <FormGroup>
+                  <Label id="lblEmail">Email:</Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.email}
+                    />
+                    {errors.email && touched.email && errors.email}
+                    </FormGroup>
+                </Col>              
+                <Col md={4}>
+                  <FormGroup>
+                    <Label id="lblPassword">Password:</Label>
+                  
+                    <Input
+                      type="password"
+                      name="password"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      value={values.password}
+                    />
+                    {errors.password && touched.password && errors.password}
+                  </FormGroup>
+                </Col>
+              </Row>
+              <Button 
+                  type="submit" 
+                  disabled={isSubmitting || !dirty}
+                  className="btn btn-default"
+                >
+                  Submit
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    );
+  }
+}
