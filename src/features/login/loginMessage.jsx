613667173d7d418206052815ffd91493f2158873
Richard Waswrick <richard.waswrick@ppdbiz.com>
2018-11-04 05:26:13 -0600

    handling authenticated redirect

--- src/features/login/loginMessage.jsx
+++ src/features/login/loginMessage.jsx
@@ -1,6 +1,15 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-export default class LoginMessage extends Component {
+class LoginMessage extends Component {
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.isAuthenticated) {
+      this.props.history.push("/home");
+    }
+  }
+
   render() {
     return (
       <div className="container">
@@ -9,3 +18,13 @@ export default class LoginMessage extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { isAuthenticated, profile } = state.auth;
+  return {
+    isAuthenticated,
+    profile
+  };
+}
+
+export default connect(mapStateToProps)(withRouter(LoginMessage));
